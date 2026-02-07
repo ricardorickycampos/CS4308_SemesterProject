dad34e6e593c81fbd92c1fb0314aeb2039cc120b
Ricardo Campos <overheadmelee@icloud.com>
2026-02-07 12:46:28 -0500

    Finished Analyzer and Token generator

--- src/Analyzer/LexicalAnalyzer.java
+++ src/Analyzer/LexicalAnalyzer.java
@@ -1,5 +1,71 @@
 package Analyzer;
 
+import globalexceptions.InvalidToken;
+
 public class LexicalAnalyzer {
 
+
+    private String sourceCode;
+    private int index = 0;
+
+    public LexicalAnalyzer(String sourceCode) {
+        this.sourceCode = sourceCode;
+    }
+
+    public Token getToken() throws InvalidToken {
+        skipWhiteSpace();
+        if (index >= sourceCode.length()) {
+            return new Token(TokenType.EOS, "", 1, index);
+        }
+        char currentChar = sourceCode.charAt(index);
+        if (Character.isDigit(currentChar)) {
+            int startIndex = index;
+            String token = readNumber();
+            return new Token(TokenType.INT_LIT, token, 1, startIndex);
+        }
+        TokenType operatorType = operatorAssign(currentChar);
+        Token token = new Token(operatorType, String.valueOf(currentChar), 1, index);
+        index++;
+        return token;
+    }
+
+    // Helper Methods
+    private String readNumber() {
+        int startIndex = index;
+        while (index < sourceCode.length() && Character.isDigit(sourceCode.charAt(index))) {
+            index++;
+        }
+        return sourceCode.substring(startIndex, index);
+    }
+    private void skipWhiteSpace() {
+        while (index < sourceCode.length() && Character.isWhitespace(sourceCode.charAt(index))) {
+            index++;
+        }
+    }
+
+    private TokenType operatorAssign(char c) throws InvalidToken {
+        switch (c) {
+            case '+' -> {
+                return TokenType.PLUS;
+            }
+            case '-' -> {
+                return TokenType.MINUS;
+            }
+            case '*' -> {
+                return TokenType.MULTIPLY;
+            }
+            case '/' -> {
+                return TokenType.DIVIDE;
+            }
+            case '(' -> {
+                return TokenType.LEFT_PAREN;
+            }
+            case ')' -> {
+                return TokenType.RIGHT_PAREN;
+            }
+            default -> {
+                throw new InvalidToken("Invalid token: " + c + " @INDEX: " + (index));
+            }
+        }
+    }
 }
--- src/Analyzer/Token.java
+++ src/Analyzer/Token.java
@@ -1,5 +1,35 @@
 package Analyzer;
 
-public enum Token {
+public class Token {
+    TokenType type;
+    String lexeme;
+    int row, col;
 
+    public Token(TokenType type, String lexeme, int row, int col) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Token for : " + lexeme + " (" + type +  ")";
+    }
 }
